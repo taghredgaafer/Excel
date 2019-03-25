f30d2df54fb3e44a372f14cddd3bb307a78b5a78
taghredgaafer22 <taghredgaafer22@outlook.com>
2019-03-25 16:32:18 +0200

    commit

--- Excel/Default.aspx
+++ Excel/Default.aspx
@@ -2,41 +2,51 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-    </div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
+    <asp:Panel ID="Panel1" runat="server">
+        <div class="jumbotron">
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Text="" />
 
+
+        </div>
+    </asp:Panel>
+
+    <asp:Panel ID="Panel2" runat="server" Visible="false">
+        <asp:Label ID="Label1" runat="server" Text="File Name"></asp:Label>
+        <asp:Label ID="lblFileName" runat="server" Text=""></asp:Label>
+
+        <asp:Label ID="Label2" runat="server" Text="Select sheet"></asp:Label>
+        <asp:DropDownList ID="ddlSheet" runat="server" AppendDataBoundItems="true"></asp:DropDownList>
+        <br />
+
+        <asp:Label ID="Label3" runat="server" Text="Enter Source Table Name" />
+
+        <asp:TextBox ID="txtTable" runat="server"> </asp:TextBox>
+
+        <br />
+
+        <asp:Label ID="Label4" runat="server" Text="Has Header Row?" />
+
+        <br />
+
+        <asp:RadioButtonList ID="rbHDR" runat="server">
+
+            <asp:ListItem Text="Yes" Value="Yes" Selected="True"> </asp:ListItem>
+
+            <asp:ListItem Text="No" Value="No"></asp:ListItem>
+
+        </asp:RadioButtonList>
+
+        <br />
+
+        <asp:Button ID="btnSave" runat="server" Text="Save"
+            OnClick="btnSave_Click" />
+
+        <asp:Button ID="btnCancel" runat="server" Text="Cancel"
+            OnClick="btnCancel_Click" />
+    </asp:Panel>
 </asp:Content>
+
--- Excel/Default.aspx.cs
+++ Excel/Default.aspx.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
 
 namespace Excel
 {
@@ -13,5 +18,226 @@ namespace Excel
         {
 
         }
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+
+            if (FileUpload1.HasFile)
+
+            {
+
+                string FileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+
+                string Extension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+
+                string FolderPath = ConfigurationManager.AppSettings["FolderPath"];
+
+                string FilePath = Server.MapPath(FolderPath + FileName);
+
+                FileUpload1.SaveAs(FilePath);
+
+                GetExcelSheets(FilePath, Extension, "Yes");
+
+            }
+
+        }
+
+        private void GetExcelSheets(string FilePath, string Extension, string IsHDR)
+        {
+            string conStr = "";
+
+            switch (Extension)
+
+            {
+
+                case ".xls": //Excel 97-03
+
+                    conStr = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+
+                    break;
+
+                case ".xlsx": //Excel 07
+
+                    conStr = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+
+                    break;
+
+                //case ".xlsx": //Excel 16
+
+                //    conStr = ConfigurationManager.ConnectionStrings["Excel16ConString"].ConnectionString;
+
+                //    break;
+
+            }
+
+
+            #region 
+
+            //Get the Sheets in Excel WorkBoo
+
+            conStr = String.Format(conStr, FilePath, IsHDR);
+
+            OleDbConnection connExcel = new OleDbConnection(conStr);
+
+            OleDbCommand cmdExcel = new OleDbCommand();
+
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+
+            cmdExcel.Connection = connExcel;
+
+            connExcel.Open();
+
+
+
+            //Bind the Sheets to DropDownList
+
+            ddlSheet.Items.Clear();
+
+            ddlSheet.Items.Add(new ListItem("--Select Sheet--", ""));
+
+            ddlSheet.DataSource = connExcel
+
+                     .GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            ddlSheet.DataTextField = "TABLE_NAME";
+
+            ddlSheet.DataValueField = "TABLE_NAME";
+
+            ddlSheet.DataBind();
+
+            connExcel.Close();
+
+            txtTable.Text = "";
+
+            lblFileName.Text = Path.GetFileName(FilePath);
+
+            Panel2.Visible = true;
+
+            Panel1.Visible = false;
+            #endregion
+        }
+
+           
+        protected void btnSave_Click(object sender, EventArgs e)
+
+        {
+
+            string FileName = lblFileName.Text;
+
+            string Extension = Path.GetExtension(FileName);
+
+            string FolderPath = Server.MapPath(ConfigurationManager.AppSettings["FolderPath"]);
+
+            string CommandText = "";
+
+            switch (Extension)
+
+            {
+
+                case ".xls": //Excel 97-03
+
+                    CommandText = "spx_ImportFromExcel03";
+
+                    break;
+
+                case ".xlsx": //Excel 07
+
+                    CommandText = "spx_ImportFromExcel07";
+
+                    break;
+
+                //case ".xlsx": //Excel 16
+
+                //    CommandText = "spx_ImportFromExcel16";
+
+                //    break;
+
+            }
+          
+
+
+            #region
+
+            //Read Excel Sheet using Stored Procedure
+
+            //And import the data into Database Table
+
+            String strConnString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(strConnString);
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.CommandText = CommandText;
+
+            cmd.Parameters.Add("@SheetName", SqlDbType.VarChar).Value =
+
+                           ddlSheet.SelectedItem.Text;
+
+            cmd.Parameters.Add("@FilePath", SqlDbType.VarChar).Value =
+
+                           FolderPath + FileName;
+
+            cmd.Parameters.Add("@HDR", SqlDbType.VarChar).Value =
+
+                           rbHDR.SelectedItem.Text;
+
+            cmd.Parameters.Add("@TableName", SqlDbType.VarChar).Value =
+
+                           txtTable.Text;
+
+            cmd.Connection = con;
+
+            try
+
+            {
+
+                con.Open();
+
+                object count = cmd.ExecuteNonQuery();
+
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+
+                lblMessage.Text = count.ToString() + " records inserted.";
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+
+                lblMessage.Text = ex.Message;
+
+            }
+
+            finally
+
+            {
+
+                con.Close();
+
+                con.Dispose();
+
+                Panel1.Visible = true;
+
+                Panel2.Visible = false;
+
+            }
+
+        }
+        #endregion
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+
+
+        }
+
+
     }
+
+
 }
